e586dcf90e1b5792a1e955525495eb68089c6685
ivine369 <ivinekemuma014@gmail.com>
2020-07-09 10:28:58 +0300

    add the file

---
+++ app/models.py
@@ -0,0 +1,22 @@
+class Sources:
+    '''
+    Sources class to define Sources objects
+    '''
+    def __init__(self, id, name, description, url, category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.subject = subject
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self, author, title, description, url, urlToImage, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
